086d0c5416c7c0d42ce227f565f629820f796346
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-11 21:44:20 +0300

    Project initialization

---
+++ src/globalStyles.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html {
+        box-sizing: border-box;
+    }
+
+    body {
+        padding: 2rem;
+        background-color: #212121;
+        color: #878787;
+        font-size: 14px;
+        font-family: 'Arial', sans-serif;
+    }
+
+    button {
+    }
+`;
+
+export default GlobalStyles;
