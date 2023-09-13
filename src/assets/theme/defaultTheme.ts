e450a6c13841c2cb6243ae58a31b1c905ad74ffd
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-13 21:28:22 +0300

    added defaultTheme

---
+++ src/assets/theme/defaultTheme.ts
@@ -0,0 +1,27 @@
+export const DefaultTheme = {
+  colors: {
+    colorButtonPrimary: '#ffffff',
+    backgroundButtonPrimary: '#ff0d29',
+    backgroundHeader: '#ffffff',
+    colorWarning: '#d10a34',
+    colorSuccess: '#52bc77',
+    colorActive: '#1e90ff',
+    borderInput: '#ededed',
+  },
+  deviceSizes: {
+    mobileSmall: '375px',
+    mobile: '576px',
+    tablet: '904px',
+    laptop: '1240px',
+  },
+  fontSizes: {
+    cardTitle: '24px',
+    cardTitleMobile: '18px',
+    cardTitleMobileSmall: '16px',
+    softSkillsTitle: '24px',
+    softSkillsTitleTablet: '22px',
+    sectionTitle: '50px',
+    sectionTitleMobile: '30px',
+    sectionTitleMobileSmall: '28px',
+  },
+};
