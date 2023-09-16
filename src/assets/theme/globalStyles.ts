2b34cf00b189574dba530a3bae27d0d0b46aaf09
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-16 19:08:02 +0300

    Added variable fonts support

--- src/assets/theme/globalStyles.ts
+++ src/assets/theme/globalStyles.ts
@@ -1,22 +1,59 @@
 import { createGlobalStyle } from 'styled-components';
+import RobotoFlexVF from '../fonts/RobotoFlexVF.woff2';
+import RobotoRegular from '../fonts/Roboto-Regular.ttf';
+import RobotoMedium from '../fonts/Roboto-Medium.ttf';
+import RobotoBold from '../fonts/Roboto-Bold.ttf';
 
 const GlobalStyles = createGlobalStyle`
-    *,
-    *::before,
-    *::after {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+  @font-face {
+    font-family: "Roboto";
+    src: url(${RobotoRegular}) format("truetype");
+    font-weight: 400;
+    font-style: normal;
+  }
 
-    html {
-        box-sizing: border-box;
-    }
+  @font-face {
+    font-family: "Roboto";
+    src: url(${RobotoMedium}) format("truetype");
+    font-weight: 500;
+    font-style: normal;
+  }
 
-    body {
-        font-size: 14px;
-        font-family: 'Arial', sans-serif;
-    }
+  @font-face {
+    font-family: "Roboto";
+    src: url(${RobotoBold}) format("truetype");
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @supports (font-variation-settings: normal) {
+    @font-face {
+    font-family: "RobotoFlex";
+    src: url(${RobotoFlexVF}) format("woff2-variations");
+    font-weight: 100 900;
+    font-stretch: 25% 151%;
+    font-style: normal;
+  };
+}
+
+  *,
+  *::before,
+  *::after {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+  }
+
+  html {
+      box-sizing: border-box;
+  }
+
+  body {
+    font-size: 14px;
+    font-family: 'RobotoFlex', 'Roboto', sans-serif;
+    font-weight: normal;
+    font-style: normal;
+  }
 `;
 
 export default GlobalStyles;
