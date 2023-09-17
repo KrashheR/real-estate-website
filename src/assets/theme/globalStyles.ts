8d179ba5a879b917d642a0fd947e152f73462e78
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-17 15:13:58 +0300

    Added new variable fonts support

--- src/assets/theme/globalStyles.ts
+++ src/assets/theme/globalStyles.ts
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
-import RobotoFlexVF from '../fonts/RobotoFlexVF.woff2';
+import RobotoFlexVFWoff from '../fonts/RobotoFlexVF.woff2';
+import RobotoFlexVFTtf from '../fonts/RobotoFlexVF.ttf';
 import RobotoRegular from '../fonts/Roboto-Regular.ttf';
 import RobotoMedium from '../fonts/Roboto-Medium.ttf';
 import RobotoBold from '../fonts/Roboto-Bold.ttf';
@@ -29,7 +30,8 @@ const GlobalStyles = createGlobalStyle`
   @supports (font-variation-settings: normal) {
     @font-face {
     font-family: "RobotoFlex";
-    src: url(${RobotoFlexVF}) format("woff2-variations");
+    src: url(${RobotoFlexVFWoff}) format("woff2-variations"),
+        url(${RobotoFlexVFTtf}) format("truetype-variations");
     font-weight: 100 900;
     font-stretch: 25% 151%;
     font-style: normal;
@@ -39,13 +41,13 @@ const GlobalStyles = createGlobalStyle`
   *,
   *::before,
   *::after {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
 
   html {
-      box-sizing: border-box;
+    box-sizing: border-box;
   }
 
   body {
@@ -53,6 +55,10 @@ const GlobalStyles = createGlobalStyle`
     font-family: 'RobotoFlex', 'Roboto', sans-serif;
     font-weight: normal;
     font-style: normal;
+  }
+
+  button {
+    font-family: 'RobotoFlex', 'Roboto', sans-serif;
   }
 `;
 
