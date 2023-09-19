322fe380888ec386fecbb02805601a7b2cb10ae7
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-19 17:20:03 +0300

    added timer component

---
+++ src/components/ui/countdownTimer/countdownTimer.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+import {
+  StyledCountdownTimerContainer,
+  StyledCountdownTimerValue,
+  StyledCountdownTimerLabel,
+} from './styled';
+
+interface Countdown {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+}
+
+interface CountdownTimerProps {
+  endDate: Date;
+}
+
+function getDifference(endDate: Date): number {
+  const now = new Date();
+  return endDate.getTime() - now.getTime();
+}
+
+function convertDifferenceToTime(difference: number): Countdown {
+  if (difference < 0) {
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  }
+
+  return {
+    days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+    hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+    minutes: Math.floor((difference / (1000 * 60)) % 60),
+    seconds: Math.floor((difference / 1000) % 60),
+  };
+}
+
+const CountdownTimer: React.FC<CountdownTimerProps> = ({ endDate }) => {
+  const getRemainingTime = (targetDate: Date) => {
+    const difference = getDifference(targetDate);
+    return convertDifferenceToTime(difference);
+  };
+
+  const [timeLeft, setTimeLeft] = useState<Countdown>(
+    getRemainingTime(endDate),
+  );
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(getRemainingTime(endDate));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [endDate]);
+
+  return (
+    <div>
+      <StyledCountdownTimerContainer>
+        <StyledCountdownTimerValue>{timeLeft.days}</StyledCountdownTimerValue>
+        <StyledCountdownTimerLabel>дней</StyledCountdownTimerLabel>
+      </StyledCountdownTimerContainer>
+      <StyledCountdownTimerContainer>
+        <StyledCountdownTimerValue>{timeLeft.hours}</StyledCountdownTimerValue>
+        <StyledCountdownTimerLabel>часов</StyledCountdownTimerLabel>
+      </StyledCountdownTimerContainer>
+      <StyledCountdownTimerContainer>
+        <StyledCountdownTimerValue>
+          {timeLeft.minutes}
+        </StyledCountdownTimerValue>
+        <StyledCountdownTimerLabel>минут</StyledCountdownTimerLabel>
+      </StyledCountdownTimerContainer>
+      <StyledCountdownTimerContainer>
+        <StyledCountdownTimerValue>
+          {timeLeft.seconds}
+        </StyledCountdownTimerValue>
+        <StyledCountdownTimerLabel>секунд</StyledCountdownTimerLabel>
+      </StyledCountdownTimerContainer>
+    </div>
+  );
+};
+
+export default CountdownTimer;
---
+++ src/components/ui/countdownTimer/styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledCountdownTimerValue = styled.span`
+  display: block;
+  font-size: 28px;
+  font-weight: 700;
+`;
+
+export const StyledCountdownTimerLabel = styled.span`
+  display: block;
+  font-size: 12px;
+`;
+
+export const StyledCountdownTimerContainer = styled.div`
+  display: inline-block;
+  margin: 0 10px;
+`;
