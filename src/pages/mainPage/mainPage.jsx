51ab51898b54d6f8bab7d5af6f02afbf828b883b
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-19 17:20:20 +0300

    added mainPage component

---
+++ src/pages/mainPage/mainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/layouts/header/header';
+import Promo from '../../components/layouts/promo/promo';
+
+function MainPage() {
+  return (
+    <>
+      <Header />
+      <Promo />
+    </>
+  );
+}
+
+export default MainPage;
