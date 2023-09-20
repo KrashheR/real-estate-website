163ff10849c3a4e08f8d9837b979a4f7cbed7d24
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-20 20:02:09 +0300

    Added theme, globalstyles supports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import styled from 'styled-components';
-import Counter from './components/Counter';
-import GlobalStyles from './globalStyles';
+import GlobalStyles from './assets/theme/globalStyles';
+import { ThemeProvider } from 'styled-components';
+import { DefaultTheme } from './assets/theme/defaultTheme';
+import MainPage from './pages/mainPage/mainPage';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={DefaultTheme}>
       <GlobalStyles />
-      <AppContainer>
-        <h1>React TypeScript styled components Webpack demo</h1>
-        <Counter />
-      </AppContainer>
-    </>
+      <MainPage />
+    </ThemeProvider>
   );
 }
 
 export default App;
-
-const AppContainer = styled.div`
-  min-height: calc(100vh - 2 * 2rem);
-  display: grid;
-  align-content: center;
-  text-align: center;
-`;
