c3bbc1326cb9feab7f5e390e2f5d5e1781306374
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-22 14:42:30 +0300

    added animations for buttons

--- src/components/ui/slideshow/styled.ts
+++ src/components/ui/slideshow/styled.ts
@@ -10,6 +10,16 @@ export const StyledSlideshow = styled.div`
   background-color: white;
   grid-area: slideshow;
 
+  &:hover{
+    .swiper-button-prev,
+    .swiper-button-next {
+      opacity: 1;
+    }
+    .swiper-button-disabled {
+      opacity: 0.35;
+    }
+  }
+
   .swiper .swiper-slide {
     background-size: cover;
     background-position: center;
@@ -20,9 +30,51 @@ export const StyledSlideshow = styled.div`
   .swiper-button-prev,
   .swiper-button-next {
     color: ${(props) => props.theme.colors.colorSlideshowButton};
+    width: 50px;
+    height: 50px;
+    background-color: white;
+    border-radius: 25%;
+    opacity: 0;
+    transition: all 0.3s ease;
+  }
+
+  .swiper-button-prev:hover,
+  .swiper-button-next:hover {
+    border-radius: 50%;
+  }
+
+  .swiper-button-prev::after,
+  .swiper-button-next::after {
+    font-size: 14px;
+    font-weight: 700;
+    align-self: center;
+    justify-self: center;
   }
 
   .swiper-pagination-bullet-active {
     background-color: ${(props) => props.theme.colors.colorSlideshowPagination};
   }
+`;
+
+export const StyledSlideshowImage = styled.img`
+  width: 540px;
+  height: 520px;
+  object-fit: cover;
+  border-radius: 25px;
+`;
+
+export const StyledSlideshowLink = styled.a`
+  display: block;
+  width: 100%;
+  height: 100%;
+
+
+`;
+
+export const StyledSlideshowTitle = styled.span`
+  position: absolute;
+  left: 10px;
+  color: ${(props) => props.theme.colors.colorSlideshowTitle};
+  font-size: 40px;
+  font-weight: 600;
 `;
