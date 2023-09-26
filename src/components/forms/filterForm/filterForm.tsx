84b9bddbb75eda38af686caf97d06a4564ba6573
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-26 17:17:38 +0300

    Added styles for form component

--- src/components/forms/filterForm/filterForm.tsx
+++ src/components/forms/filterForm/filterForm.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import { StyledFilterForm } from './styled';
+import {
+  StyledFilterForm,
+  StyledFormLabel,
+  StyledFormItem,
+} from './styled';
+import FormSelect from './formSelect/formSelect';
+import FormRange from './formRange/formRange';
+import FormRadio from './formRadio/formRadio';
 
 function FilterForm() {
   return (
     <StyledFilterForm>
-      <input type="text" />
-      <select>
-        <option>Новостройки</option>
-        <option>Машиноместа</option>
-      </select>
+      <StyledFormItem>
+        <StyledFormLabel htmlFor="form-object-type">
+          Тип объекта
+        </StyledFormLabel>
+        <FormSelect id="form-object-type" />
+      </StyledFormItem>
+      <StyledFormItem>
+        <StyledFormLabel htmlFor="form-object-price">
+          Стоимость объекта, млн. руб.
+        </StyledFormLabel>
+        <FormRange id="form-object-price"/>
+      </StyledFormItem>
+      <StyledFormItem>
+        <StyledFormLabel htmlFor="form-object-date">
+          Сдача объекта
+        </StyledFormLabel>
+        <FormRadio />
+      </StyledFormItem>
     </StyledFilterForm>
   );
-};
+}
 
 export default FilterForm;
--- src/components/forms/filterForm/styled.ts
+++ src/components/forms/filterForm/styled.ts
@@ -1,5 +1,18 @@
 import styled from "styled-components";
 
 export const StyledFilterForm = styled.form`
-  height: 100px;
+  display: flex;
+  justify-content: space-between;
+  padding: 50px 0;
+`;
+
+export const StyledFormLabel = styled.label`
+  font-size: 14px;
+  padding-bottom: 8px;
+`;
+
+export const StyledFormItem = styled.span`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
 `;
