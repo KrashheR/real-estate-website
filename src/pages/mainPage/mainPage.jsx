8ac2c3d7bb4f17faf5ee5297e5ad8902a557f556
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-27 18:27:21 +0300

    Added projects section

--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../components/layouts/header/header';
 import Promo from '../../components/layouts/promo/promo';
+import Projects from '../../components/layouts/projects/projects';
 
 function MainPage() {
   return (
     <>
       <Header />
       <Promo />
+      <Projects />
     </>
   );
 }
