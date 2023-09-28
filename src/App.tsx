309d45bd3ddc228862bd04876f6ceffd6eb253af
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-28 18:21:28 +0300

    added error boundary wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import GlobalStyles from './assets/theme/globalStyles';
 import { ThemeProvider } from 'styled-components';
 import { DefaultTheme } from './assets/theme/defaultTheme';
 import MainPage from './pages/mainPage/mainPage';
+import ErrorBoundary from './components/errorBoundary/errorBoundary';
 
 function App() {
   return (
-    <ThemeProvider theme={DefaultTheme}>
-      <GlobalStyles />
-      <MainPage />
-    </ThemeProvider>
+    <ErrorBoundary>
+      <ThemeProvider theme={DefaultTheme}>
+        <GlobalStyles />
+        <MainPage />
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 }
 
