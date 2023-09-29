b78fa56ab48fcfdaca3394e2249e4a1acfc7f79a
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-09-29 21:39:09 +0300

    Added display of available apartments and parking

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-import { StyledCard } from "./styled";
+import React from 'react';
+import { StyledCard, StyledCardAvaiable, StyledCardAvaiableContainer, StyledCardButton } from './styled';
+import Title, { TitleLevel, TitleColor, TitleType } from '../title/title';
+import ReadMoreButton from '../readMoreButton/readMoreButton';
+
+interface CardProps {
+  gridArea: string;
+}
 
 function Card() {
-  return(
+  return (
     <StyledCard>
+      <Title
+        level={TitleLevel.H2}
+        color={TitleColor.WHITE}
+        type={TitleType.CARD}
+      >
+        Card Name
+      </Title>
 
+      <StyledCardAvaiableContainer>
+        <StyledCardAvaiable $area={"house"}>20 квартир</StyledCardAvaiable>
+        <StyledCardAvaiable $area={"housePrice"}>от 10 млн.</StyledCardAvaiable>
+        <StyledCardAvaiable $area={"parking"}>10 машиномест</StyledCardAvaiable>
+        <StyledCardAvaiable $area={"parkingPrice"}>от 1 млн.</StyledCardAvaiable>
+        <StyledCardButton>ПОДРОБНЕЕ</StyledCardButton>
+      </StyledCardAvaiableContainer>
     </StyledCard>
   );
 }
--- src/components/ui/card/styled.ts
+++ src/components/ui/card/styled.ts
@@ -1,5 +1,36 @@
 import styled from "styled-components";
+import ReadMoreButton from "../readMoreButton/readMoreButton";
+import { StyledReadMoreButton } from "../readMoreButton/styled";
 
-export const StyledCard = styled.div`
+type StyledPromoCardProps = {
+  $area: string;
+};
 
+
+export const StyledCard = styled.a`
+  display: block;
+  width: 370px;
+  height: 410px;
+  border-radius: 20px;
+  background-color: ${(props) => props.theme.colors.colorMainRed};
+`;
+
+export const StyledCardAvaiableContainer = styled.div`
+  display: grid;
+  grid-template-areas: 'house housePrice button'
+                        'parking parkingPrice button';
+  align-items: center;
+  padding: 20px;
+
+`;
+
+export const StyledCardButton = styled(StyledReadMoreButton)`
+  grid-area: button;
+  justify-self: end;
+`;
+
+export const StyledCardAvaiable = styled.span<StyledPromoCardProps>`
+  grid-area: ${(props) => props.$area};
+  font-size: 12px;
+  color: ${(props) => props.theme.colors.colorWhite};
 `;
