0d501edd043a74e74b58e60a1029991b370f487d
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-03 21:25:11 +0300

    Updated animations

--- src/components/layouts/promo/promoCard/styled.ts
+++ src/components/layouts/promo/promoCard/styled.ts
@@ -7,8 +7,8 @@ type StyledPromoCardProps = {
 export const StyledPromoCard = styled.a<StyledPromoCardProps>`
   display: flex;
   flex-direction: column;
+  grid-area: ${(props) => props.$area};
   padding: 25px;
-  justify-content: space-between;
   width: 280px;
   height: 240px;
   border-radius: 25px;
@@ -16,17 +16,21 @@ export const StyledPromoCard = styled.a<StyledPromoCardProps>`
   color: ${(props) => props.theme.colors.colorBlack};
   background-color: ${(props) => props.theme.colors.colorWhite};
   text-decoration: none;
-  grid-area: ${(props) => props.$area};
-  transition: all 0.3s ease;
   box-shadow: 0px 0px 8px 4px rgba(238, 228, 228, 0.2);
+  transition:
+    transform 0.3s ease,
+    box-shadow 0.3s ease,
+    color 0.1s ease;
 
   &:hover {
-    transform: scale3d(1.01, 1.01, 1) translateY(-4px);
-    box-shadow: 0px 4px 8px 4px rgba(238, 228, 228, 0.4);
+    color: ${(props) => props.theme.colors.colorMainRed};
+    transform: scale3d(1.01, 1.01, 1) translateY(-2px);
+    box-shadow: 0px 2px 8px 4px rgba(238, 228, 228, 0.7);
   }
 `;
 
 export const StyledPromoImg = styled.img`
   width: 200px;
   height: 80px;
+  margin-top: auto;
 `;
