2062d36cf463b82fd9aa522168af8e1ceb8fce05
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-03 21:24:25 +0300

    Added api for getting card data

--- src/components/layouts/projects/projectsList/projectsList.tsx
+++ src/components/layouts/projects/projectsList/projectsList.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyledProjectsList } from "./styled";
-import Card from "../../../ui/card/card";
+import Card, { CardType } from "../../../ui/card/card";
 
 function ProjectsList() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://www.krashher.ru/real-estate/api/get-cards.php")
+    .then(response => response.json())
+    .then(data => setData(data));
+  },[])
+
   return(
     <StyledProjectsList>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card type={CardType.PREMIUM}/>
+      <Card type={CardType.PREMIUM}/>
+      <Card type={CardType.PREMIUM}/>
+      <Card type={CardType.STANDART}/>
+      <Card type={CardType.STANDART}/>
+      <Card type={CardType.STANDART}/>
+      <Card type={CardType.STANDART}/>
     </StyledProjectsList>
   );
 }
