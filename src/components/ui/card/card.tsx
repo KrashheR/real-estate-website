88323908544294ef013c8c2108d2c5c76553c5e1
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-04 14:07:32 +0300

    added display of parking spaces from json

--- src/components/ui/card/card.tsx
+++ src/components/ui/card/card.tsx
@@ -15,42 +15,59 @@ import humanPremium from '../../../assets/images/card/humanPremium.svg';
 import metro from '../../../assets/images/card/metro.svg';
 import human from '../../../assets/images/card/human.svg';
 
-
 export enum CardType {
   PREMIUM = 'premium',
   STANDART = 'standart',
 }
 
-interface CardProps {
-  type: CardType;
-}
+type Card = {
+  data: CardProps;
+};
 
-function Card({type}: CardProps) {
+type CardProps = {
+  type: CardType;
+  href: string;
+  image: string;
+  title: string;
+  parking: string;
+  apartments: string;
+};
+
+function Card({ data }: Card) {
+
+  let oneRoom = JSON.parse(data.apartments);
+  console.log(oneRoom["1"]);
+  const parking = JSON.parse(data.parking);
+
   return (
-    <StyledCard $type={type}>
+    <StyledCard $type={data.type}>
       <StyledCardTitleContainer>
         <Title
           level={TitleLevel.H2}
-          color={type === CardType.PREMIUM ? TitleColor.WHITE : TitleColor.GRAY}
+          color={data.type === CardType.PREMIUM ? TitleColor.WHITE : TitleColor.GRAY}
           type={TitleType.CARD}
         >
-          CARD NAME
+          {data.title}
         </Title>
         <StyledCardPlaceContainer>
-          <StyledCardPlaceIcon src={type === CardType.PREMIUM ? metroPremium : metro} />
+          <StyledCardPlaceIcon
+            src={data.type === CardType.PREMIUM ? metroPremium : metro}
+          />
           <StyledCardPlaceText>Щелковская</StyledCardPlaceText>
-          <StyledCardPlaceIcon src={type === CardType.PREMIUM ? humanPremium : human} />
+          <StyledCardPlaceIcon
+            src={data.type === CardType.PREMIUM ? humanPremium : human}
+          />
           <StyledCardPlaceText>от 10 мин.</StyledCardPlaceText>
         </StyledCardPlaceContainer>
       </StyledCardTitleContainer>
       <StyledCardAvaiableContainer>
         <StyledCardAvaiable $area={'house'}>20 квартир</StyledCardAvaiable>
         <StyledCardAvaiable $area={'housePrice'}>от 10 млн.</StyledCardAvaiable>
-        <StyledCardAvaiable $area={'parking'}>10 машиномест</StyledCardAvaiable>
+        <StyledCardAvaiable $area={'parking'}>{parking.num} машиномест</StyledCardAvaiable>
         <StyledCardAvaiable $area={'parkingPrice'}>
-          от 1 млн.
+          от {parking.price} млн.
         </StyledCardAvaiable>
-        <StyledCardButton $type={type}>ПОДРОБНЕЕ</StyledCardButton>
+        <StyledCardButton $type={data.type}>ПОДРОБНЕЕ</StyledCardButton>
       </StyledCardAvaiableContainer>
     </StyledCard>
   );
