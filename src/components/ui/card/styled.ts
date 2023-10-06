2c0fd18cba869f3abd0aecc65da01346c4488693
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-06 17:06:51 +0300

    updated card styles

--- src/components/ui/card/styled.ts
+++ src/components/ui/card/styled.ts
@@ -1,5 +1,5 @@
-import styled, { withTheme } from "styled-components";
-import { StyledReadMoreButton } from "../readMoreButton/styled";
+import styled from 'styled-components';
+import { StyledReadMoreButton } from '../readMoreButton/styled';
 
 type StyledCardAreaProps = {
   $area: string;
@@ -16,23 +16,47 @@ export const StyledCard = styled.a<StyledCardTypeProps>`
   gap: 20px;
   width: 370px;
   height: 410px;
-  border-radius: 20px;
+  border-radius: 15px;
   background-color: ${(props) => {
-    return props.$type === "premium" ? props.theme.colors.colorMainRed : props.theme.colors.colorLightRed;
+    return props.$type === 'premium'
+      ? props.theme.colors.colorMainRed
+      : props.theme.colors.colorLightRed;
   }};
   color: ${(props) => {
-    return props.$type === "premium" ? props.theme.colors.colorWhite : props.theme.colors.colorBlack;
+    return props.$type === 'premium'
+      ? props.theme.colors.colorWhite
+      : props.theme.colors.colorBlack;
   }};
-  padding: 20px;
+
   cursor: pointer;
+  transition: all 0.4s ease;
+
+  &:hover {
+    box-shadow: ${(props) => {
+      return props.$type === 'premium'
+        ? '0px 5px 8px 0px rgba(184, 41, 57, 0.7)'
+        : '0px 5px 8px 0px rgba(195, 194, 194, 0.7)';
+    }};
+    transform: translateY(-2px);
+  }
+`;
+
+export const StyledCardImage = styled.img`
+  display: block;
+  height: 323px;
+  width: 100%;
+  object-fit: cover;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
 `;
 
 export const StyledCardAvaiableContainer = styled.div`
   display: grid;
-  grid-template-areas: 'house housePrice button'
-                        'parking parkingPrice button';
+  grid-template-areas:
+    'house housePrice button'
+    'parking parkingPrice button';
   align-items: center;
-
+  padding: 0 20px 20px 20px;
 `;
 
 export const StyledCardPlaceContainer = styled.div`
@@ -47,25 +71,31 @@ export const StyledCardPlaceIcon = styled.img`
 
 export const StyledCardPlaceText = styled.span`
   font-size: 14px;
-
 `;
 
 export const StyledCardTitleContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 3px;
+  padding: 0 20px;
 `;
 
-export const StyledCardButton = styled(StyledReadMoreButton)<StyledCardTypeProps>`
+export const StyledCardButton = styled(
+  StyledReadMoreButton,
+)<StyledCardTypeProps>`
   grid-area: button;
   justify-self: end;
   height: 40px;
 
   background-color: ${(props) => {
-    return props.$type === "premium" ? props.theme.colors.colorWhite : props.theme.colors.colorMainRed;
+    return props.$type === 'premium'
+      ? props.theme.colors.colorWhite
+      : props.theme.colors.colorMainRed;
   }};
   color: ${(props) => {
-    return props.$type === "premium" ? props.theme.colors.colorMainRed : props.theme.colors.colorWhite;
+    return props.$type === 'premium'
+      ? props.theme.colors.colorMainRed
+      : props.theme.colors.colorWhite;
   }};
 `;
 
