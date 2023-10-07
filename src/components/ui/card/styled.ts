8349d0c37815190280ef95ddaab7449f3b799dcf
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-07 20:36:35 +0300

    Fixed border and box-shadows

--- src/components/ui/card/styled.ts
+++ src/components/ui/card/styled.ts
@@ -13,41 +13,37 @@ export const StyledCard = styled.a<StyledCardTypeProps>`
   display: flex;
   flex-direction: column;
   justify-content: end;
-  gap: 20px;
   width: 370px;
   height: 410px;
-  border-radius: 15px;
-  background-color: ${(props) => {
-    return props.$type === 'premium'
-      ? props.theme.colors.colorMainRed
-      : props.theme.colors.colorLightRed;
-  }};
-  color: ${(props) => {
-    return props.$type === 'premium'
-      ? props.theme.colors.colorWhite
-      : props.theme.colors.colorBlack;
-  }};
-
+  color: ${(props) => props.$type === 'premium' ? props.theme.colors.colorWhite : props.theme.colors.colorBlack};
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
   cursor: pointer;
-  transition: all 0.4s ease;
-
+  transition: all 0.3s ease-in-out;
+  border-radius: 15px;
   &:hover {
-    box-shadow: ${(props) => {
-      return props.$type === 'premium'
-        ? '0px 5px 8px 0px rgba(184, 41, 57, 0.7)'
-        : '0px 5px 8px 0px rgba(195, 194, 194, 0.7)';
-    }};
-    transform: translateY(-2px);
+    transform: translateY(-5px);
+    box-shadow: 0px 12px 20px rgba(0, 0, 0, 0.15);
   }
 `;
 
 export const StyledCardImage = styled.img`
   display: block;
-  height: 323px;
+  height: 263px;
   width: 100%;
   object-fit: cover;
   border-top-left-radius: 15px;
   border-top-right-radius: 15px;
+`;
+
+export const StyledCardDescription = styled.div<StyledCardTypeProps>`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  gap: 20px;
+  height: 147px;
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+  background-color: ${(props) => props.$type === 'premium' ? props.theme.colors.colorMainRed : props.theme.colors.colorLightRed};
 `;
 
 export const StyledCardAvaiableContainer = styled.div`
