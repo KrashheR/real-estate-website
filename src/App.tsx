aabb5ecd25039b37acc68eeeed5c649469c9619a
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-10 00:17:42 +0300

    added redux support

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { ThemeProvider } from 'styled-components';
 import { DefaultTheme } from './assets/theme/defaultTheme';
 import MainPage from './pages/mainPage/mainPage';
 import ErrorBoundary from './components/errorBoundary/errorBoundary';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
     <ErrorBoundary>
-      <ThemeProvider theme={DefaultTheme}>
-        <GlobalStyles />
-        <MainPage />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={DefaultTheme}>
+          <GlobalStyles />
+          <MainPage />
+        </ThemeProvider>
+      </Provider>
     </ErrorBoundary>
   );
 }
