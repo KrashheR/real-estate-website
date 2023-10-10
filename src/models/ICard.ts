46267d4e0d89bda1c131cc0d9efa10847c8070de
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-10 16:27:44 +0300

    Added ICard interface

---
+++ src/models/ICard.ts
@@ -0,0 +1,12 @@
+export interface ICard {
+  apartments: string;
+  deadline: string;
+  href: string;
+  id: string;
+  image: string;
+  metro: string;
+  parking: string;
+  title: string;
+  type: string;
+  walkTime: number;
+}
