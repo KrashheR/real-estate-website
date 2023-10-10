76f75be9b5ff75a6189fc5406876df2cc397c068
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-10 16:26:22 +0300

    Added setupStore

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { setupStore } from './store/store';
 import App from './App';
+
+const store = setupStore();
 
 ReactDOM.render(
   <React.StrictMode>
