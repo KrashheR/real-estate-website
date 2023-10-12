5474e0aee3ba940ef80e01d210b34a45037cddb3
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-12 18:02:49 +0300

    removed usefull code

--- src/components/layouts/projects/projectsList/projectsList.tsx
+++ src/components/layouts/projects/projectsList/projectsList.tsx
@@ -1,42 +1,15 @@
 import React from 'react';
 import { StyledProjectsList } from "./styled";
 import Card from "../../../ui/card/card";
-import { ICard } from '../../../../models/ICard';
 import { useAppSelector } from '../../../../hooks/redux';
-import { selectSortedCards } from '../../../../store/reducers/Selectors';
+import { selectFilteredByTypeCards } from '../../../../store/reducers/Selectors';
 
-
-interface FilterValues {
-  minPrice: number | null;
-  maxPrice: number | null;
-  objectType: string;
-  deliveryDate: number | null;
-}
-
-interface ProjectsListProps {
-  filters: FilterValues;
-}
-
-
-const filterData = (data: ICard[], filters: FilterValues): ICard[] => {
-  return data.filter(item => {
-    if (filters.deliveryDate !== null) {
-      const deadlineYear = parseInt(item.deadline, 10);
-      if (deadlineYear !== filters.deliveryDate) {
-        return false;
-      }
-    }
-    return true;
-  });
-};
-
-function ProjectsList({ filters }: ProjectsListProps) {
-  const sortedCards = useAppSelector(selectSortedCards);
-  const filteredData = filterData(sortedCards, filters);
+function ProjectsList() {
+  const filteredCards = useAppSelector(selectFilteredByTypeCards);
 
   return (
     <StyledProjectsList>
-      {filteredData.map((item) => {
+      {filteredCards.map((item) => {
         return (
           <Card data={item} key={item.id} />
         );
