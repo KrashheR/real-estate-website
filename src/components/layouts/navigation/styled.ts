49e8a2bff17b3f816152a32e9fd480461414001c
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-16 17:24:18 +0300

    added component

---
+++ src/components/layouts/navigation/styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledNavigation = styled.nav`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  justify-content: space-between;
+  gap: 20px;
+`;
