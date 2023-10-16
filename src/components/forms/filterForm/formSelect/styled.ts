bb5961e5a43105054619d822fa8f520594a9434b
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-16 17:24:06 +0300

    improved styles

--- src/components/forms/filterForm/formSelect/styled.ts
+++ src/components/forms/filterForm/formSelect/styled.ts
@@ -1,32 +1,35 @@
 import styled from "styled-components";
+import DropdownArrow from "../../../../assets/images/forms/dropdown-arrow.svg"
 
 export const StyledFormSelect = styled.select`
   overflow: hidden;
   height: 60px;
-  width: 340px;
-  border: none;
-  border-radius: 15px;
+  width: 370px;
+  border: 1px solid ${(props) => props.theme.colors.colorLightRed};
+  border-radius: 25px;
   padding: 20px;
   font-size: 16px;
   font-weight: 500;
   color: ${(props) => props.theme.colors.colorBlack};
+  background-image: url(${DropdownArrow});
+  background-repeat: no-repeat;
+  background-position: right 20px center;
+  background-size: 25px;
   background-color: ${(props) => props.theme.colors.colorLightRed};
+  box-shadow: 0px 2px 4px ${(props) => props.theme.colors.boxShadowMain};
   transition: all 0.3s ease;
   cursor: pointer;
-  appearance: button;
+  appearance: none;
   outline: none;
 
-  &:hover{
-    border-radius: 30px;
+  &:hover {
+    box-shadow: 0px 2px 6px ${(props) => props.theme.colors.boxShadowHover};
   }
 
-  &:focus {
-    outline: 1px solid ${(props) => props.theme.colors.colorMainRed};
-  }
 `;
 
 export const StyledFormSelectOption = styled.option`
-  padding: 10px;
+  padding: 10px 20px;
   outline: none;
   border: none;
 `;
