121be2a5b4f70940e09774315ebce9d2ebd68bd8
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-17 14:12:56 +0300

    added component content

--- src/components/layouts/footer/footer.tsx
+++ src/components/layouts/footer/footer.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { StyledFooter } from "./styled";
-
+import { StyledFooter, StyledFooterProjectsList, StyledFooterProject, StyledFooterProjectLink, StyledFooterContainer } from "./styled";
+import Logo from "../../ui/logo/logo";
+import logoImage from '../../../assets/images/logo.svg';
+import { useAppSelector } from "../../../hooks/redux";
+import { selectCards } from "../../../store/reducers/Selectors";
 
 function Footer() {
+  const cards = useAppSelector(selectCards);
+
+
   return(
     <StyledFooter>
-
+      <StyledFooterContainer>
+        <Logo srcImage={logoImage} />
+        <StyledFooterProjectsList>
+          Наши проекты:
+          {cards.map((card) => {
+            return(
+              <StyledFooterProject key={card.id}>
+                <StyledFooterProjectLink href={card.href}>
+                  {card.title}
+                </StyledFooterProjectLink>
+              </StyledFooterProject>
+            );
+          })}
+        </StyledFooterProjectsList>
+      </StyledFooterContainer>
     </StyledFooter>
   );
 }
--- src/components/layouts/footer/styled.ts
+++ src/components/layouts/footer/styled.ts
@@ -1,6 +1,37 @@
 import styled from "styled-components";
+import { StyledContainer } from "../container/styled";
 
 export const StyledFooter = styled.footer`
-  display: flex;
+  margin-top: 40px;
+  height: 100%;
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.colorLightRed};
+`;
 
+export const StyledFooterContainer = styled(StyledContainer)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+`;
+
+export const StyledFooterProjectsList = styled.ul`
+  display: flex;
+  gap: 20px;
+  font-weight: 600;
+`;
+
+export const StyledFooterProject = styled.li`
+  list-style: none;
+  font-weight: 400;
+`;
+
+export const StyledFooterProjectLink = styled.a`
+  text-decoration: none;
+  color: ${(props) => props.theme.colors.colorBlack};
+  transition: all 0.3s ease;
+
+  &:hover{
+    color: ${(props) => props.theme.colors.colorMainRed};
+  }
 `;
