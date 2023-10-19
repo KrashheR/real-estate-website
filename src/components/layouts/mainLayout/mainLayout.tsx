442159f9b89a4c9c90559fab507a8393cbf09bde
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-19 17:30:50 +0300

    added component

---
+++ src/components/layouts/mainLayout/mainLayout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+
+function MainLayout() {
+
+  return(
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+export default MainLayout;
