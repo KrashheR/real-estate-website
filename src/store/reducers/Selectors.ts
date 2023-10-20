e67bfec83840dca33e220717292fca195ce25e05
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-20 12:29:25 +0300

    added selector by title

--- src/store/reducers/Selectors.ts
+++ src/store/reducers/Selectors.ts
@@ -1,27 +1,24 @@
 import { createSelector } from 'reselect';
 import { RootState } from '../store';
+import { ApartmentType } from '../../types/ICard';
 
 export const selectCards = (state: RootState) => state.cardReducer.cards;
 
-interface ApartmentType {
-  num: number;
-  price: number;
-}
+export const selectSortedCards = createSelector([selectCards], (cards) => {
+  return [...cards].sort((a, b) => {
+    if (a.type === 'premium' && b.type !== 'premium') {
+      return -1;
+    }
+    if (a.type !== 'premium' && b.type === 'premium') {
+      return 1;
+    }
+    return 0;
+  });
+});
 
-export const selectSortedCards = createSelector(
-  [selectCards],
-  (cards) => {
-    return [...cards].sort((a, b) => {
-      if (a.type === "premium" && b.type !== "premium") {
-        return -1;
-      }
-      if (a.type !== "premium" && b.type === "premium") {
-        return 1;
-      }
-      return 0;
-    });
-  }
-);
+export const selectCardByTitle = (state: RootState, title: string) => {
+  return state.cardReducer.cards.find((card) => card.title === title);
+};
 
 export const selectFilteredCards = createSelector(
   [selectSortedCards, (state: RootState) => state.cardReducer.filters],
@@ -33,7 +30,8 @@ export const selectFilteredCards = createSelector(
       isMatch = true;
 
       if (deliveryDate !== null) {
-        isMatch = parseInt(card.deliveryDate, 10) === deliveryDate ? true : false;
+        isMatch =
+          parseInt(card.deliveryDate, 10) === deliveryDate ? true : false;
       }
 
       const apartments = JSON.parse(card.apartments);
@@ -41,43 +39,56 @@ export const selectFilteredCards = createSelector(
       let keyToCheck: string | null = null;
 
       if (objectType !== 'Все объекты' && isMatch) {
-        switch(objectType) {
+        switch (objectType) {
           case 'Однокомнатная квартира':
-            isMatch = Object.values(apartments['1']).some((apartmentType) => (apartmentType as ApartmentType).num > 0);
-            keyToCheck = "1";
+            isMatch = Object.values(apartments['1']).some(
+              (apartmentType) => (apartmentType as ApartmentType).num > 0,
+            );
+            keyToCheck = '1';
             break;
           case 'Двухкомнатная квартира':
-            isMatch = Object.values(apartments['2']).some((apartmentType) => (apartmentType as ApartmentType).num > 0);
-            keyToCheck = "2";
+            isMatch = Object.values(apartments['2']).some(
+              (apartmentType) => (apartmentType as ApartmentType).num > 0,
+            );
+            keyToCheck = '2';
             break;
           case 'Трёхкомнатная квартира':
-            isMatch = Object.values(apartments['3']).some((apartmentType) => (apartmentType as ApartmentType).num > 0);
-            keyToCheck = "3";
+            isMatch = Object.values(apartments['3']).some(
+              (apartmentType) => (apartmentType as ApartmentType).num > 0,
+            );
+            keyToCheck = '3';
             break;
           case 'Машиноместо':
-            keyToCheck = "parking";
+            keyToCheck = 'parking';
             isMatch = parking.num > 0;
             break;
         }
       }
 
-      if (minPrice !== null || maxPrice !== null && isMatch) {
-        const isPriceInRange = (price: number, minPrice?: number, maxPrice?: number): boolean =>
-        price >= (minPrice ?? price) && price <= (maxPrice ?? price);
+      if (isMatch) {
+        const isPriceInRange = (
+          price: number,
+          minPrice: number,
+          maxPrice: number,
+        ): boolean =>
+          price >= (minPrice ?? price) && price <= (maxPrice ?? price);
 
         const isApartmentAvailable = (apartmentType: ApartmentType): boolean =>
-        apartmentType.num > 0 && isPriceInRange(apartmentType.price, minPrice, maxPrice);
+          apartmentType.num > 0 &&
+          isPriceInRange(apartmentType.price, minPrice, maxPrice);
 
         if (keyToCheck && apartments[keyToCheck]) {
-          isMatch = Object.values(apartments[keyToCheck])
-            .some(apartmentType => isApartmentAvailable(apartmentType as ApartmentType));
-        } else if (keyToCheck === "parking") {
+          isMatch = Object.values(apartments[keyToCheck]).some(
+            (apartmentType) =>
+              isApartmentAvailable(apartmentType as ApartmentType),
+          );
+        } else if (keyToCheck === 'parking') {
           isMatch = isPriceInRange(parking.price, minPrice, maxPrice);
         } else {
-          isMatch = Object.values(apartments).some(apartment =>
-            Object.values(apartment as ApartmentType).some(apartmentType =>
-              isApartmentAvailable(apartmentType as ApartmentType)
-            )
+          isMatch = Object.values(apartments).some((apartment) =>
+            Object.values(apartment as ApartmentType).some((apartmentType) =>
+              isApartmentAvailable(apartmentType as ApartmentType),
+            ),
           );
         }
       }
@@ -86,5 +97,5 @@ export const selectFilteredCards = createSelector(
     });
 
     return filteredCards;
-  }
+  },
 );
