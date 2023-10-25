c6aeb7b93aac57e5721fd6bde47cfaa99674c5c7
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-25 19:52:18 +0300

    added router link for logo

--- src/components/layouts/navigation/navigation.tsx
+++ src/components/layouts/navigation/navigation.tsx
@@ -3,11 +3,12 @@ import City from '../../ui/city/city';
 import Logo from '../../ui/logo/logo';
 import logoImage from '../../../assets/images/logo.svg';
 import NavigationUl from './navigationUl/navigationUl';
+import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
     <StyledNavigation>
-      <Logo srcImage={logoImage} />
+      <Link to="/"><Logo srcImage={logoImage} /></Link>
       <NavigationUl />
       <City />
     </StyledNavigation>
