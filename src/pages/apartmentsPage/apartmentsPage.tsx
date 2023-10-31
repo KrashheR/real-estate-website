e2179358e4156f9a96cbca026ee9cba6e81aaa27
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-10-31 18:43:49 +0300

    updated page styles

--- src/pages/apartmentsPage/apartmentsPage.tsx
+++ src/pages/apartmentsPage/apartmentsPage.tsx
@@ -1,23 +1,28 @@
-import { useParams } from "react-router-dom";
-import { useAppSelector } from "../../hooks/redux";
-import { selectCardByTitle } from "../../store/reducers/Selectors";
-import Apartment from "../../components/layouts/apartment/apartment";
-import Catalog from "../../components/layouts/catalog/catalog";
+import { useParams } from 'react-router-dom';
+import { useAppSelector } from '../../hooks/redux';
+import { selectCardByTitle } from '../../store/reducers/Selectors';
+import Apartment from '../../components/layouts/apartment/apartment';
+import Title, { TitleLevel, TitleType } from '../../components/ui/title/title';
+import Projects from '../../components/layouts/projects/projects';
+import { StyledApartmentsContainer } from './styled';
 
 function ApartmentsPage() {
   let { title } = useParams<{ title: string }>();
+  const card = useAppSelector((state) =>
+    selectCardByTitle(state, title as string),
+  );
 
-  if (!title) {
-    return <Catalog />;
-  }
-  const card = useAppSelector(state => selectCardByTitle(state, title as string));
-
-  if (!card) {
-    return <Catalog />;
+  if (card) {
+    return <Apartment data={card} />;
   }
 
   return (
-    <Apartment data={card}/>
+    <StyledApartmentsContainer>
+      <Title level={TitleLevel.H1} type={TitleType.APARTMENTMAP}>
+        Каталог новостроек:
+      </Title>
+      <Projects />
+    </StyledApartmentsContainer>
   );
 }
 
