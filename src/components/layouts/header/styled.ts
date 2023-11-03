e328f1b199f117e9ca1cd25a7c3a19aaa51d7d04
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-03 22:52:21 +0300

    added new styles

--- src/components/layouts/header/styled.ts
+++ src/components/layouts/header/styled.ts
@@ -5,4 +5,6 @@ export const StyledHeader = styled.header`
   top: 0px;
   z-index: 10;
   background-color: ${(props) => props.theme.colors.backgroundHeader};
+  border-bottom: 1px solid ${(props) => props.theme.colors.colorLightRed};
+  box-shadow: 0 4px 4px rgba(0,0,0,.05);
 `;
