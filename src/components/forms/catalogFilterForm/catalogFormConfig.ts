cf9bb4c8938aba14b56e3bac4bbde108d7178aab
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-06 22:02:27 +0300

    added parking

--- src/components/forms/catalogFilterForm/catalogFormConfig.ts
+++ src/components/forms/catalogFilterForm/catalogFormConfig.ts
@@ -1,6 +1,6 @@
 export const catalogOptions = [
+  { value: 'parking', label: 'Машиноместа', id: 'parking' },
   { value: '1', label: '1', id: 'rooms-1' },
   { value: '2', label: '2', id: 'rooms-2' },
   { value: '3', label: '3', id: 'rooms-3' },
-  { value: '4', label: '4', id: 'rooms-4' },
 ]
