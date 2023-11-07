2e1453020a52636d7588693078cad87224241c78
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-07 21:22:28 +0300

    added styles for catalog info

--- src/components/layouts/catalog/catalogInfo/catalogInfo.tsx
+++ src/components/layouts/catalog/catalogInfo/catalogInfo.tsx
@@ -1,29 +1,57 @@
-import { CatalogDetails } from "../../../../types/ICatalog";
-import { StyledCatalogInfo, StyledCatalogInfoData, StyledCatalogInfoItem, StyledCatalogInfoLabel, StyledCatalogInfoContainer, StyledCatalogInfoImg } from "./styled";
+import { CatalogDetails } from '../../../../types/ICatalog';
+import {
+  StyledCatalogInfoApartment,
+  StyledCatalogInfoData,
+  StyledCatalogInfoItem,
+  StyledCatalogInfoLabel,
+  StyledCatalogInfoContainer,
+  StyledCatalogInfoImg,
+  StyledCatalogInfoPlaceholder,
+  StyledCatalogInfoPlaceholderTitle,
+} from './styled';
 
 interface CatalogInfoProps {
   selectedApartment: CatalogDetails | null;
 }
 
-function CatalogInfo({selectedApartment}: CatalogInfoProps) {
-
-  if(selectedApartment){
-    return(
-      <StyledCatalogInfo>
-          <StyledCatalogInfoImg src={selectedApartment.image} alt="План квартиры" />
-          <StyledCatalogInfoContainer>
-            <StyledCatalogInfoItem><StyledCatalogInfoLabel>Цена</StyledCatalogInfoLabel><StyledCatalogInfoData>{selectedApartment.price}</StyledCatalogInfoData></StyledCatalogInfoItem>
-            <StyledCatalogInfoItem><StyledCatalogInfoLabel>Площадь</StyledCatalogInfoLabel><StyledCatalogInfoData>{selectedApartment.size} м²</StyledCatalogInfoData></StyledCatalogInfoItem>
-          </StyledCatalogInfoContainer>
-      </StyledCatalogInfo>
+function CatalogInfo({ selectedApartment }: CatalogInfoProps) {
+  if (selectedApartment) {
+    return (
+      <StyledCatalogInfoApartment>
+        <StyledCatalogInfoImg
+          src={selectedApartment.image}
+          alt="План квартиры"
+        />
+        <StyledCatalogInfoContainer>
+          <StyledCatalogInfoItem>
+            <StyledCatalogInfoLabel>Площадь</StyledCatalogInfoLabel>
+            <StyledCatalogInfoData>
+              {selectedApartment.size} м²
+            </StyledCatalogInfoData>
+          </StyledCatalogInfoItem>
+          <StyledCatalogInfoItem>
+            <StyledCatalogInfoLabel>Этаж</StyledCatalogInfoLabel>
+            <StyledCatalogInfoData>{selectedApartment.floor}</StyledCatalogInfoData>
+          </StyledCatalogInfoItem>
+          <StyledCatalogInfoItem>
+            <StyledCatalogInfoLabel>Цена</StyledCatalogInfoLabel>
+            <StyledCatalogInfoData>
+              {selectedApartment.price * 1000000} ₽
+            </StyledCatalogInfoData>
+          </StyledCatalogInfoItem>
+          <StyledCatalogInfoItem>
+            <StyledCatalogInfoLabel>В ипотеку</StyledCatalogInfoLabel>
+            <StyledCatalogInfoData>
+              от {(selectedApartment.price * 1000 * 12).toFixed(0)} ₽/мес.
+            </StyledCatalogInfoData>
+          </StyledCatalogInfoItem>
+        </StyledCatalogInfoContainer>
+      </StyledCatalogInfoApartment>
     );
   }
 
-  return(
-    <StyledCatalogInfo>
-      Выберите понравившийся план квартиры
-    </StyledCatalogInfo>
-
+  return (
+    <StyledCatalogInfoPlaceholder><StyledCatalogInfoPlaceholderTitle>Выберите понравившийся план квартиры</StyledCatalogInfoPlaceholderTitle></StyledCatalogInfoPlaceholder>
   );
 }
 
--- src/components/layouts/catalog/catalogInfo/styled.ts
+++ src/components/layouts/catalog/catalogInfo/styled.ts
@@ -1,18 +1,34 @@
 import styled from "styled-components";
 
 export const StyledCatalogInfo = styled.div`
-  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
   width: 880px;
   height: 600px;
+  border-radius: 30px;
+  padding: 20px;
+  background-color: ${(props) => props.theme.colors.colorWhite};
+`;
 
+export const StyledCatalogInfoApartment = styled(StyledCatalogInfo)`
+  justify-content: space-between;
+`;
+
+export const StyledCatalogInfoPlaceholder = styled(StyledCatalogInfo)`
+  justify-content: center;
+`;
+
+export const StyledCatalogInfoPlaceholderTitle = styled.h3`
+  display: block;
+  font-weight: 400;
+  font-size: 20px;
 `;
 
 export const StyledCatalogInfoImg = styled.img`
-  width: 480px;
-  height: 400px;
+  width: 520px;
+  height: 460px;
 `;
 
 export const StyledCatalogInfoContainer = styled.span`
@@ -20,11 +36,8 @@ export const StyledCatalogInfoContainer = styled.span`
   align-items: center;
   justify-content: center;
   gap: 20px;
-  position: absolute;
-  top: 70%;
-  left:0;
-  width: 100%;
-  padding: 20px;
+  width: fit-content;
+  padding: 20px 40px;
   border-radius: 30px;
   background-color: ${(props) => props.theme.colors.colorLightRed};
 `;
@@ -32,12 +45,16 @@ export const StyledCatalogInfoContainer = styled.span`
 export const StyledCatalogInfoItem = styled.span`
   display: inline-flex;
   flex-direction: column;
+  align-items: center;
+  gap: 5px;
 `;
 
 export const StyledCatalogInfoLabel = styled.p`
-
+  font-size: 12px;
+  color: ${(props) => props.theme.colors.colorGray}
 `;
 
 export const StyledCatalogInfoData = styled.p`
-
+  color: ${(props) => props.theme.colors.colorBlack};
+  font-weight: 500;
 `
