12ed23a05490ee19deaa62513047f74ff063464e
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-07 21:29:24 +0300

    components have been renamed

--- src/components/layouts/projects/projectsList/styled.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const StyledProjectsList = styled.div`
-  display: grid;
-  grid-gap: 45px;
-  grid-template-columns: repeat(auto-fit, minmax(370px, 1fr));
-`;
