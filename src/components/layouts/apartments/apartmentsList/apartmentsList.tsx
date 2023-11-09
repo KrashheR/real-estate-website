e7c28882b1cf4339d29492079a2ec1eb74bee39d
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-09 12:01:52 +0300

    added placeholder for empty card list

--- src/components/layouts/apartments/apartmentsList/apartmentsList.tsx
+++ src/components/layouts/apartments/apartmentsList/apartmentsList.tsx
@@ -1,4 +1,4 @@
-import { StyledApartmentsList } from "./styled";
+import { StyledApartmentsList, StyledApartmentsListPlaceholderTitle } from "./styled";
 import Card from "../../../ui/card/card";
 import { useAppSelector } from '../../../../hooks/redux';
 import { selectFilteredCards } from '../../../../store/reducers/Selectors';
@@ -13,6 +13,7 @@ function ApartmentsList() {
           <Card data={item} key={item.id} />
         );
       })}
+      {filteredCards.length === 0 && <StyledApartmentsListPlaceholderTitle>Нет квартир, соответствующих вашим фильтрам</StyledApartmentsListPlaceholderTitle>}
     </StyledApartmentsList>
   );
 }
--- src/components/layouts/apartments/apartmentsList/styled.ts
+++ src/components/layouts/apartments/apartmentsList/styled.ts
@@ -4,4 +4,12 @@ export const StyledApartmentsList = styled.div`
   display: grid;
   grid-gap: 45px;
   grid-template-columns: repeat(auto-fit, minmax(370px, 1fr));
+  min-height: 200px;
+`;
+
+export const StyledApartmentsListPlaceholderTitle = styled.h3`
+  font-weight: 400;
+  text-align: center;
+  padding-top: 50px;
+  font-size: 18px;
 `;
