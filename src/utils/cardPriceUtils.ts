1d3c353386513459167b327b0f8d60e773084ceb
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-10 21:29:13 +0300

    function updated to match new cardApartments

--- src/utils/cardPriceUtils.ts
+++ src/utils/cardPriceUtils.ts
@@ -1,17 +1,22 @@
-import { ApartmentType, ICard } from "../types/ICard";
+import { ICard, ParkingDetails, ResidentialDetails } from "../types/ICard";
 
 export const getMinAndMaxApartmentPrice = (cards: ICard[]): [number, number] => {
   let minPrice = Infinity;
   let maxPrice = -Infinity;
 
   cards.forEach(card => {
-    const apartments = JSON.parse(card.apartments);
-    Object.values(apartments).forEach((apartment) => {
-      Object.values(apartment as ApartmentType).forEach(apartmentType => {
-        const price = parseFloat(apartmentType.price);
-        if (price < minPrice) minPrice = price;
-        if (price > maxPrice) maxPrice = price;
-      });
+    const cardApartments = JSON.parse(card.apartments);
+    const apartmentItems: ResidentialDetails[] = cardApartments.apartments;
+    const parkingItems: ParkingDetails[] = cardApartments.parking;
+
+    apartmentItems.forEach(apartment => {
+      if (apartment.price < minPrice) minPrice = apartment.price;
+      if (apartment.price > maxPrice) maxPrice = apartment.price;
+    });
+
+    parkingItems.forEach(parking => {
+      if (parking.price < minPrice) minPrice = parking.price;
+      if (parking.price > maxPrice) maxPrice = parking.price;
     });
   });
 
