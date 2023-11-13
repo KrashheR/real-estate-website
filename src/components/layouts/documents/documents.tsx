3750d6efae10c59da8b1db573cf2b81172ba62fb
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-13 13:10:11 +0300

    added document cards mapping

--- src/components/layouts/documents/documents.tsx
+++ src/components/layouts/documents/documents.tsx
@@ -2,22 +2,33 @@ import Description, { DescriptionType } from '../../ui/description/description';
 import Title, { TitleType, TitleLevel } from '../../ui/title/title';
 import DocumentCard from './documentCard/documentCard';
 import { StyledDocuments, StyledDocumentsCards, StyledDocumentsHeader } from './styled';
+import downloadImg from "../../../assets/images/documents/download.svg";
+import { DocumentsData } from '../../../types/ICard';
 
-function Documents() {
+interface DocumentsProps {
+  documents : string;
+}
+
+function Documents({documents}:DocumentsProps) {
+  const documentsData : DocumentsData[] = JSON.parse(documents);
+
   return (
     <StyledDocuments>
       <StyledDocumentsHeader>
-        <Title level={TitleLevel.H2} type={TitleType.APARTMENT}>
+        <Title level={TitleLevel.H2} type={TitleType.APARTMENTSECTION}>
           Документы проекта
         </Title>
-        <Description type={DescriptionType.APARTMENT}>
+        <Description type={DescriptionType.DOCUMENTS}>
           Все документы находятся в открытом доступе. Здесь вы можете
           ознакомиться со всеми необходимыми документами по проекту.
         </Description>
       </StyledDocumentsHeader>
       <StyledDocumentsCards>
-        <DocumentCard title={"Документы по проекту"} linkText={"Скачать документы"}/>
-        <DocumentCard title={"Гид по проекту"} linkText={"Скачать гида"}/>
+        {documentsData.map((item) => {
+          return(
+            <DocumentCard title={item.title} downloadLink={item.downloadLink} downloadText={item.downloadText} imageSrc={downloadImg} key={item.documentId}/>
+          );
+        })}
       </StyledDocumentsCards>
     </StyledDocuments>
   );
--- src/components/layouts/documents/styled.ts
+++ src/components/layouts/documents/styled.ts
@@ -3,10 +3,13 @@ import styled from "styled-components";
 export const StyledDocuments = styled.section`
   display: flex;
   flex-direction: column;
+  gap: 40px;
+  padding: 30px 0;
 `;
 
 export const StyledDocumentsHeader = styled.div`
   display: flex;
+  justify-content: space-between;
 `;
 
 export const StyledDocumentsCards = styled.div`
