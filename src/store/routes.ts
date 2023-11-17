cdf7cba474a547fa27ec293b68db4bbc6c63cf69
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-17 13:29:55 +0300

    routes to api moved to new file

---
+++ src/store/routes.ts
@@ -0,0 +1,5 @@
+export const getSlideshowUrl = "https://krashher.ru/real-estate/api/get-slideshow.php";
+
+export const getCardsUrl = "https://www.krashher.ru/real-estate/api/get-cards.php";
+
+export const getNewsUrl = "https://www.krashher.ru/real-estate/api/get-news.php";
