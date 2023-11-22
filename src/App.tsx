4c1f247585609548d0171c4070589cd7e5cfcb02
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-22 13:18:59 +0300

    new pages have been added to the router

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import GlobalStyles from './assets/theme/globalStyles';
 import { ThemeProvider } from 'styled-components';
 import { DefaultTheme } from './assets/theme/defaultTheme';
 import MainPage from './pages/mainPage/mainPage';
-import ErrorBoundary from './components/errorBoundary/errorBoundary';
+import ErrorBoundary from './utils/errorBoundary/errorBoundary';
 import { useAppDispatch } from './hooks/redux';
 import { useEffect } from 'react';
 import { fetchCards, fetchPromos } from './store/reducers/ActionCreators';
 import { Routes, Route } from "react-router-dom";
 import ApartmentsPage from './pages/apartmentsPage/apartmentsPage' ;
 import MainLayout from './components/layouts/mainLayout/mainLayout';
-import About from './pages/about/about';
+import About from './pages/aboutPage/aboutPage';
+import NewsPage from './pages/newsPage/newsPage';
+import NewsArticle from './pages/newsArticle/newsArticle';
+import ConstructorPage from './pages/constructorPage/constructorPage';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -27,8 +30,11 @@ function App() {
           <Route path="/" element={<MainLayout />} >
             <Route index path='/' element={<MainPage />}/>
             <Route path='apartments' element={<ApartmentsPage />}/>
-            <Route path='apartments/:title/' element={<ApartmentsPage />}/>
-            <Route path='*' element={<About />}/>
+            <Route path='apartments/:id/' element={<ApartmentsPage />}/>
+            <Route path='about' element={<About />}/>
+            <Route path='news' element={<NewsPage />}/>
+            <Route path='news/:id/' element={<NewsArticle/>}/>
+            <Route path='constructor/' element={<ConstructorPage/>}/>
           </Route>
         </Routes>
       </ThemeProvider>
