171545179cb5057bca4b6d1ed1ee545d26b80b4d
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-22 13:20:00 +0300

    selectors were separated into different files

--- src/store/reducers/CardSelectors.ts
+++ src/store/reducers/CardSelectors.ts
@@ -4,7 +4,10 @@ import { IApartmentData } from '../../types/ICard';
 
 export const selectCards = (state: RootState) => state.cardReducer.cards;
 
-export const selectPromos = (state: RootState) => state.promoReducer.promos;
+export const selectCardById = (state: RootState, id: string) => {
+  console.log();
+  return state.cardReducer.cards.find((card) => card.id === id);
+};
 
 export const selectMinPrice = (state: RootState) =>
   state.cardReducer.filters.minPrice;
@@ -23,17 +26,14 @@ export const selectSortedCards = createSelector([selectCards], (cards) => {
   });
 });
 
-export const selectCardByTitle = (state: RootState, title: string) => {
-  return state.cardReducer.cards.find((card) => card.title === title);
-};
-
 export const selectFilteredCards = createSelector(
   [selectSortedCards, (state: RootState) => state.cardReducer.filters],
   (sortedCards, filters) => {
     const { deliveryDate, objectType, minPrice, maxPrice } = filters;
 
     const filteredCards = sortedCards.filter((card) => {
-      let isMatch: boolean = deliveryDate === null || card.deliveryDate === deliveryDate;
+      let isMatch: boolean =
+        deliveryDate === null || card.deliveryDate === deliveryDate;
 
       const cardApartments: IApartmentData[] = JSON.parse(card.apartments);
 
@@ -41,31 +41,38 @@ export const selectFilteredCards = createSelector(
         price: number,
         minPrice: number | null,
         maxPrice: number | null,
-      ): boolean => price >= (minPrice ?? price) && price <= (maxPrice ?? price);
+      ): boolean =>
+        price >= (minPrice ?? price) && price <= (maxPrice ?? price);
 
       const isApartmentAvailable = (apartment: IApartmentData): boolean =>
-        apartment.num > 0 && isPriceInRange(apartment.price, minPrice, maxPrice);
+        apartment.num > 0 &&
+        isPriceInRange(apartment.price, minPrice, maxPrice);
 
       if (isMatch) {
         switch (objectType) {
           case 'one-room':
             isMatch = cardApartments.some(
-              (apartment) => apartment.roomNum === '1' && isApartmentAvailable(apartment),
+              (apartment) =>
+                apartment.roomNum === '1' && isApartmentAvailable(apartment),
             );
             break;
           case 'two-room':
             isMatch = cardApartments.some(
-              (apartment) => apartment.roomNum === '2' && isApartmentAvailable(apartment),
+              (apartment) =>
+                apartment.roomNum === '2' && isApartmentAvailable(apartment),
             );
             break;
           case 'three-room':
             isMatch = cardApartments.some(
-              (apartment) => apartment.roomNum === '3' && isApartmentAvailable(apartment),
+              (apartment) =>
+                apartment.roomNum === '3' && isApartmentAvailable(apartment),
             );
             break;
           case 'parking-space':
             isMatch = cardApartments.some(
-              (apartment) => apartment.roomNum === 'parking' && isApartmentAvailable(apartment),
+              (apartment) =>
+                apartment.roomNum === 'parking' &&
+                isApartmentAvailable(apartment),
             );
             break;
           default:
@@ -80,25 +87,3 @@ export const selectFilteredCards = createSelector(
     return filteredCards;
   },
 );
-
-
-const selectNewsState = (state: RootState) => state.newsReducer;
-
-export const selectNews = createSelector(
-  [selectNewsState],
-  (newsState) => newsState.news
-);
-
-export const selectNewsById = (state: RootState, id: number) => {
-  return state.newsReducer.news.find((item) => item.id == id);
-}
-
-export const selectNewsCount = createSelector(
-  [selectNewsState],
-  (newsState) => newsState.count
-);
-
-export const selectNewsLoading = createSelector (
-  [selectNewsState],
-  (newsState) => newsState.isLoading
-)
---
+++ src/store/reducers/NewsSelectors.ts
@@ -0,0 +1,33 @@
+import { createSelector } from 'reselect';
+import { RootState } from '../store';
+
+const selectNewsState = (state: RootState) => state.newsReducer;
+
+export const selectNews = createSelector(
+  [selectNewsState],
+  (newsState) => newsState.news,
+);
+
+export const selectCurrentPage = createSelector(
+  [selectNewsState],
+  (newsState) => newsState.currentPage,
+);
+
+export const selectPageSize = createSelector(
+  [selectNewsState],
+  (newsState) => newsState.pageSize,
+);
+
+export const selectTotalNews = createSelector(
+  [selectNewsState],
+  (newsState) => newsState.total,
+);
+
+export const selectNewsById = (state: RootState, id: number) => {
+  return state.newsReducer.news.find((item) => item.id == id);
+};
+
+export const selectNewsLoading = createSelector(
+  [selectNewsState],
+  (newsState) => newsState.isLoading,
+);
