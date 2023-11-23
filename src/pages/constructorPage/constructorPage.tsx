7d6fffef84e4671aaea76e72cf7c3848f2923bdb
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-23 11:10:41 +0300

    updated constructor page

--- src/pages/constructorPage/constructorPage.tsx
+++ src/pages/constructorPage/constructorPage.tsx
@@ -1,5 +1,4 @@
 import Constructor from '../../components/ui/constructor/constructor';
-import Title, { TitleLevel, TitleType } from '../../components/ui/title/title';
 import { StyledConstructorContainer, StyledConstructorPage } from './styled';
 import { constructorOptions } from './constructorConfig';
 
@@ -8,9 +7,6 @@ function ConstructorPage() {
   return (
     <StyledConstructorPage>
       <StyledConstructorContainer>
-        <Title level={TitleLevel.H1} type={TitleType.PAGETITLE}>
-          Подобрать квартиру мечты
-        </Title>
         <Constructor data={constructorOptions}/>
       </StyledConstructorContainer>
     </StyledConstructorPage>
