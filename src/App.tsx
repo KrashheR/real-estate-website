0c34a257f9f8918cf1a4e801113507713fa2fe6b
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-11-26 11:02:38 +0300

    updated paths for buildings pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ import { useAppDispatch } from './hooks/redux';
 import { useEffect } from 'react';
 import { fetchBuildings, fetchPromos } from './store/reducers/ActionCreators';
 import { Routes, Route } from "react-router-dom";
-import ApartmentsPage from './pages/apartmentsPage/apartmentsPage' ;
+import BuildingsPage from './pages/buildingsPage/buildingsPage' ;
 import MainLayout from './components/layouts/mainLayout/mainLayout';
 import About from './pages/aboutPage/aboutPage';
 import NewsPage from './pages/newsPage/newsPage';
 import NewsArticle from './pages/newsArticle/newsArticle';
 import ConstructorPage from './pages/constructorPage/constructorPage';
+import ApartmentPage from './pages/apartmentPage/apartmentPage';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -29,8 +30,8 @@ function App() {
         <Routes>
           <Route path="/" element={<MainLayout />} >
             <Route index path='/' element={<MainPage />}/>
-            <Route path='apartments' element={<ApartmentsPage />}/>
-            <Route path='apartments/:id/' element={<ApartmentsPage />}/>
+            <Route path='buildings' element={<BuildingsPage />}/>
+            <Route path='buildings/:id/' element={<ApartmentPage />}/>
             <Route path='about' element={<About />}/>
             <Route path='news' element={<NewsPage />}/>
             <Route path='news/:id/' element={<NewsArticle/>}/>
