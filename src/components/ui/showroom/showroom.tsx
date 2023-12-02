6fec101845fbce8781033244c11c22236c05c233
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-12-02 13:32:04 +0300

    added support of mobile images

--- src/components/ui/showroom/showroom.tsx
+++ src/components/ui/showroom/showroom.tsx
@@ -1,11 +1,15 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { StyledShowroom, StyledShowroomButtons, StyledShowroomImage } from './styled';
+import {
+  StyledShowroom,
+  StyledShowroomButtons,
+  StyledShowroomImage,
+  StyledShowroomPicture,
+} from './styled';
 import ShowroomButton from './showroomButton/showroomButton';
 import { useState, useRef } from 'react';
 import SwiperCore from 'swiper';
 import { IRoomSlides } from '../../../types/IBuilding';
-
 
 export interface ShowroomProps {
   slides: IRoomSlides[];
@@ -28,26 +32,65 @@ function Showroom({ slides }: ShowroomProps) {
 
   function handleRoomSelection(roomType: string) {
     const swiper = swiperRef.current;
-    if(swiper) {
-      const targetSlideIndex = slides.findIndex((slide) => slide.type === roomType);
-      if(targetSlideIndex !== -1) {
+    if (swiper) {
+      const targetSlideIndex = slides.findIndex(
+        (slide) => slide.type === roomType,
+      );
+      if (targetSlideIndex !== -1) {
         swiper.slideTo(targetSlideIndex);
       }
     }
-  };
+  }
 
-  return(
+  return (
     <StyledShowroom>
       <StyledShowroomButtons>
-        <ShowroomButton selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} room="livingroom" onButtonClick={handleRoomSelection}>Гостиная</ShowroomButton>
-        <ShowroomButton selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} room="bedroom" onButtonClick={handleRoomSelection}>Спальня</ShowroomButton>
-        <ShowroomButton selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} room="kitchen" onButtonClick={handleRoomSelection}>Кухня</ShowroomButton>
-        <ShowroomButton selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} room="bathroom" onButtonClick={handleRoomSelection}>Ванная</ShowroomButton>
-        <ShowroomButton selectedRoom={selectedRoom} setSelectedRoom={setSelectedRoom} room="hallway" onButtonClick={handleRoomSelection}>Прихожая</ShowroomButton>
+        <ShowroomButton
+          selectedRoom={selectedRoom}
+          setSelectedRoom={setSelectedRoom}
+          room="livingroom"
+          onButtonClick={handleRoomSelection}
+        >
+          Гостиная
+        </ShowroomButton>
+        <ShowroomButton
+          selectedRoom={selectedRoom}
+          setSelectedRoom={setSelectedRoom}
+          room="bedroom"
+          onButtonClick={handleRoomSelection}
+        >
+          Спальня
+        </ShowroomButton>
+        <ShowroomButton
+          selectedRoom={selectedRoom}
+          setSelectedRoom={setSelectedRoom}
+          room="kitchen"
+          onButtonClick={handleRoomSelection}
+        >
+          Кухня
+        </ShowroomButton>
+        <ShowroomButton
+          selectedRoom={selectedRoom}
+          setSelectedRoom={setSelectedRoom}
+          room="bathroom"
+          onButtonClick={handleRoomSelection}
+        >
+          Ванная
+        </ShowroomButton>
+        <ShowroomButton
+          selectedRoom={selectedRoom}
+          setSelectedRoom={setSelectedRoom}
+          room="hallway"
+          onButtonClick={handleRoomSelection}
+        >
+          Прихожая
+        </ShowroomButton>
       </StyledShowroomButtons>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
-        onSwiper={(swiper) => { swiperRef.current = swiper; }}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
         spaceBetween={50}
         slidesPerView={1}
         navigation
@@ -57,13 +100,15 @@ function Showroom({ slides }: ShowroomProps) {
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index} data-type={slide.type}>
-            <StyledShowroomImage src={slide.image} alt="" />
+            <StyledShowroomPicture>
+              <source srcSet={slide.imageMobile} media="(max-width: 576px)" />
+              <StyledShowroomImage src={slide.image} />
+            </StyledShowroomPicture>
           </SwiperSlide>
         ))}
       </Swiper>
     </StyledShowroom>
   );
-
-};
+}
 
 export default Showroom;
