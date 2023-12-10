3b08c6d8888c472c19c07afb4ad3bb1e96848309
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-12-11 00:06:24 +0300

    added placeholder for empty buildings list

---
+++ src/pages/buildingsPage/buildingsList/buildingListEmpty/buildingListEmpty.tsx
@@ -0,0 +1,18 @@
+import Title, { TitleLevel, TitleSize, TitleWeight } from "../../../../components/ui/title/title";
+import { StyledBuildingsListEmpty } from "./styled";
+
+function BuildingListEmpty() {
+  return (
+    <StyledBuildingsListEmpty>
+      <Title
+        level={TitleLevel.H3}
+        size={TitleSize.S}
+        weight={TitleWeight.NORMAL}
+      >
+        Нет квартир, соответствующих вашим фильтрам
+      </Title>
+    </StyledBuildingsListEmpty>
+  );
+}
+
+export default BuildingListEmpty;
---
+++ src/pages/buildingsPage/buildingsList/buildingListEmpty/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledBuildingsListEmpty = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 450px;
+  text-align: center;
+`;
