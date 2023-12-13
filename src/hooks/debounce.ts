358a70089f29387e72809ccffc3469109be9a3cf
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2023-12-13 17:42:12 +0300

    added useDebouncedFunction hook

---
+++ src/hooks/debounce.ts
@@ -0,0 +1,17 @@
+import { useRef } from "react";
+
+export default function useDebouncedFunction<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  const ref = useRef<NodeJS.Timeout | null>(null);
+
+  return (...args: Parameters<T>) => {
+    if (ref.current) {
+      clearTimeout(ref.current);
+    }
+    ref.current = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
