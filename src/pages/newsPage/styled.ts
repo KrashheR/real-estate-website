f153a0c6f4c949501eaf8be488bae0fb41a1b75d
Nikita Kucherencko <nikita.kucherencko.ya@yandex.ru>
2025-05-12 21:35:49 +0300

    - [fixed] news page styles

--- src/pages/newsPage/styled.ts
+++ src/pages/newsPage/styled.ts
@@ -1,10 +1,11 @@
-import styled from "styled-components";
-import { StyledContainer } from "../../components/layouts/container/styled";
+import styled from 'styled-components';
+import { StyledContainer } from '../../components/layouts/container/styled';
 
 export const StyledNewsPage = styled(StyledContainer)`
   padding-top: 30px;
   padding-bottom: 30px;
   color: ${(props) => props.theme.colors.colorBlack};
+  min-height: calc(100vh - 200px);
 `;
 
 export const StyledNewsButton = styled.div`
